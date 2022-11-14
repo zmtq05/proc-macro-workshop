70f403b12b74f847719b37012927c273163ad619
zmtq05 <zmtq05@gmail.com>
2022-11-14 21:23:13 +0900

    builder-3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,8 +10,29 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_ident = Ident::new(&format!("{}Builder", ident), ident.span());
     let syn::Data::Struct(s) = input.data else { unimplemented!() };
     let field_idents = s.fields.iter().map(|f| &f.ident);
-    let field_idents2 = field_idents.clone();
     let field_types = s.fields.iter().map(|f| &f.ty);
+
+    let f_idents = field_idents.clone();
+    let f_types = field_types.clone();
+
+    let builder_definition = quote! {
+        pub struct #builder_ident {
+            #(#f_idents: Option<#f_types>,)*
+        }
+    };
+
+    let f_idents = field_idents.clone();
+    let f_types = field_types.clone();
+    let builder_impl = quote! {
+        impl #builder_ident {
+            #(
+            fn #f_idents(&mut self, #f_idents: #f_types) -> &mut Self {
+                self.#f_idents = Some(#f_idents);
+                self
+            }
+            )*
+        }
+    };
 
     let output = quote! {
         impl #ident {
@@ -22,9 +43,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
 
-        pub struct #builder_ident {
-            #(#field_idents2: Option<#field_types>,)*
-        }
+        #builder_definition
+
+        #builder_impl
     };
 
     output.into()
